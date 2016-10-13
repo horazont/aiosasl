98080078c49253cabfa5c28fce2b4b22029d049a
Jonas Wielicki <j.wielicki@sotecware.net>
2016-10-13 17:07:52 +0200

    Configure the theme a little

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,21 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "github_button": "true",
+    "github_repo": "aiosasl",
+    "github_user": "horazont",
+    "font_size": "12pt",
+}
+html_sidebars = {
+    '**': [
+        'about.html',
+        'localtoc.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
